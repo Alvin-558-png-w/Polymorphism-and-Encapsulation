31d55b7fe24d3124e83785f03543bfe4273584fb
Alvin Nderui <alvin.nderui@gmail.com>
2024-11-20 08:42:24 +0300

    Add files via upload

---
+++ act2.py
@@ -0,0 +1,13 @@
+class computer:
+    def __init__(self):
+        self._maxprice=500
+    def sell(self):
+        print("selling price is", self._maxprice)
+    def setmaxprice(self,price):
+        self._setmaxprice
+c=computer()
+c.sell()
+c._setmaxprice=799
+c.sell()
+c._setmaxprice(799)
+c.sell
